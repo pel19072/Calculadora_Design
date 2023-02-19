5f89cdba84625cd6efaba162e1ab16db93f3d383
mor19213 <mor19213@uvg.edu.gt>
2023-02-18 20:01:57 -0600

    agregar archivo de pruebas

---
+++ pruebas.py
@@ -0,0 +1,13 @@
+from modulos import calculadora
+
+calc = calculadora()
+print(calc.multiplicar([3,2,1,4]))
+print(calc.multiplicar([5,32,4.2]))
+print(calc.dividir([3,3,2]))
+print(calc.dividir([6,2,3]))
+print(calc.sumar([3,4,2]))
+print(calc.sumar([3,4,2]))
+print(calc.restar([3,4,2]))
+print(calc.restar([3,4,2]))
+print(calc)
+
